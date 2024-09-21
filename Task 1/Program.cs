ec1ef652de3dfdfafdf28a23cb5c91c2cfc7a4a1
Sorokovsky <Sorokovskys@ukr.net>
2024-09-21 14:56:09 +0300

    example

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,8 +1,49 @@
 ﻿//Variant 11
+namespace Task_1;
 public static class Program
 {
     public static void Main()
     {
+        Console.WriteLine("Enter a car.");
+        Vehicle vehicle = EnterCar();
+        vehicle.Show();
+        Console.WriteLine("Enter a train.");
+        IShowable train = EnterTrain();
+        train.Show();
+        Console.WriteLine("Enter a express.");
+        IShowable express = EnterExpress();
+        express.Show();
+    }
 
+    public static Car EnterCar()
+    {
+        string mark;
+        int fuelMax;
+        string name;
+        int speed;
+        Console.WriteLine("Введіть ім'я: "); name = Console.ReadLine();
+        Console.WriteLine("Введіть швидкість: "); speed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть бренд: "); mark = Console.ReadLine();
+        Console.WriteLine("Введіть максимальну кількість палива: "); fuelMax = Convert.ToInt32(Console.ReadLine());
+        return new Car(name, speed, mark, fuelMax);
+    }
+
+    public static Train EnterTrain()
+    {
+        int carriageCount;
+        string name;
+        int speed;
+        Console.WriteLine("Введіть ім'я: "); name = Console.ReadLine();
+        Console.WriteLine("Введіть швидкість: "); speed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введіть кількість: "); carriageCount = Convert.ToInt32(Console.ReadLine());
+        return new Train(name, speed, carriageCount);
+    }
+
+    public static Express EnterExpress()
+    {
+        string benefits;
+        Console.WriteLine("Введіть переваги: "); benefits = Console.ReadLine();
+        Train train = EnterTrain();
+        return new Express(train, benefits);
     }
 }
